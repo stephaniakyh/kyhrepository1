486b8097cbbfa0cf93d17d988b45568cccdd5fd6
stephania <kaithie@naver.com>
2018-07-26 16:13:22 +0900

    20180726PM파이썬실습 로또추가

--- lotto.py
+++ lotto.py
@@ -20,17 +20,46 @@
 
 
 #
+
+
+# import random
+
+# # #로또번호는 1~46, 총6개 당첨번호
+# today = []
+
+# today.append(random.randrange(1,46,1))
+# today.append(random.randrange(1,46,1))
+# today.append(random.randrange(1,46,1))
+# today.append(random.randrange(1,46,1))
+# today.append(random.randrange(1,46,1))
+# today.append(random.randrange(1,46,1))
+
+# print("오늘의 추첨번호는 " 
+# + str(today[0]) + ", "
+# + str(today[1]) + ", "
+# + str(today[2]) + ", "
+# + str(today[3]) + ", "
+# + str(today[4]) + ", "
+# + str(today[5]) + "입니다")
+# start=0
+# while start<6:
+#     today.append(random.randrange(1,46,1))
+#     start=start+1
+
 import random
 
 # #로또번호는 1~46, 총6개 당첨번호
-today = []
+# today = []
 
-today.append(random.randrange(1,46,1))
-today.append(random.randrange(1,46,1))
-today.append(random.randrange(1,46,1))
-today.append(random.randrange(1,46,1))
-today.append(random.randrange(1,46,1))
-today.append(random.randrange(1,46,1))
+# today.append(random.randrange(1,46,1))
+# today.append(random.randrange(1,46,1))
+# today.append(random.randrange(1,46,1))
+# today.append(random.randrange(1,46,1))
+# today.append(random.randrange(1,46,1))
+# today.append(random.randrange(1,46,1))
+
+
+today=random.sample(range(1,47),6)
 
 print("오늘의 추첨번호는 " 
 + str(today[0]) + ", "
@@ -39,7 +68,3 @@ print("오늘의 추첨번호는 "
 + str(today[3]) + ", "
 + str(today[4]) + ", "
 + str(today[5]) + "입니다")
-start=0
-while start<6:
-    today.append(random.randrange(1,46,1))
-    start=start+1
