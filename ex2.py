031a4affcf7693c681d0488d8df1ea99640c4755
stephania <kaithie@naver.com>
2018-07-26 15:49:29 +0900

    20180726파이썬실습

---
+++ ex2.py
@@ -0,0 +1,22 @@
+# from calculator import add
+
+# result = calculator.add(1,2)
+# print(result)
+
+#
+# from calculator import add
+
+# result = add(1,2)
+# print(result)
+
+#
+from calculator import add,sub,mul,div
+# from calculator import *
+result = add(1,2)
+print(result)
+result = sub(7,3)
+print(result)
+result = mul(20,11)
+print(result)
+result = div(10,4)
+print(result)
---
+++ lotto.py
@@ -0,0 +1,45 @@
+# import random
+
+# #로또번호는 1~46, 총6개 당첨번호
+# today = []
+
+# today.append(random.randrange(1,46,1))
+# today.append(random.randrange(1,46,1))
+# today.append(random.randrange(1,46,1))
+# today.append(random.randrange(1,46,1))
+# today.append(random.randrange(1,46,1))
+# today.append(random.randrange(1,46,1))
+
+# print("오늘의 추첨번호는 " 
+# + str(today[0]) + ", "
+# + str(today[1]) + ", "
+# + str(today[2]) + ", "
+# + str(today[3]) + ", "
+# + str(today[4]) + ", "
+# + str(today[5]) + "입니다")
+
+
+#
+import random
+
+# #로또번호는 1~46, 총6개 당첨번호
+today = []
+
+today.append(random.randrange(1,46,1))
+today.append(random.randrange(1,46,1))
+today.append(random.randrange(1,46,1))
+today.append(random.randrange(1,46,1))
+today.append(random.randrange(1,46,1))
+today.append(random.randrange(1,46,1))
+
+print("오늘의 추첨번호는 " 
++ str(today[0]) + ", "
++ str(today[1]) + ", "
++ str(today[2]) + ", "
++ str(today[3]) + ", "
++ str(today[4]) + ", "
++ str(today[5]) + "입니다")
+start=0
+while start<6:
+    today.append(random.randrange(1,46,1))
+    start=start+1
